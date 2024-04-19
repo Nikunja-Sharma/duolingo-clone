f55723f9555a15d2b158ac87b78b8f2f644d8124
Nikunja-Sharma <xnikunja@gmail.com>
2024-04-19 15:27:38 +0530

    customized button component

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,30 @@ import Image from "next/image";
 export default function Home() {
     return (
         <>
-            <h1 className="text-green-500 font-bold">How are you?</h1>
-            <Button>Click me </Button>
+            <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+                <Button variant="default">e </Button>
+                <Button className="" variant="primary">
+                    Click me
+                </Button>
+                <Button variant="primaryOutline">Click me </Button>
+                <Button className="" variant="secondary">
+                    Click me
+                </Button>
+                <Button variant="secondaryOutline">secondary</Button>
+                <Button className="" variant="danger">
+                    Click me
+                </Button>
+                <Button variant="dangerOutline">secondary</Button>
+                <Button className="" variant="super">
+                    Click me
+                </Button>
+                <Button variant="superOutline">secondary</Button>
+                <Button variant="ghost">secondary</Button>
+                <Button className="" variant="sidebar">
+                    Click me
+                </Button>
+                <Button variant="sidebarOutline">secondary</Button>
+            </div>
         </>
     );
 }
